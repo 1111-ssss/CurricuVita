3f37f1123a570ed2daeba46c957dece57fe93338
1111-ssss <gurskievgeni@gmail.com>
2026-09-03 14:36:13 +0300

    refactor(identity): change interface methods

--- src/Domain/Interfaces/Identity/IIdentityService.cs
+++ src/Domain/Interfaces/Identity/IIdentityService.cs
@@ -1,20 +1,27 @@
 using Domain.Entities;
+using Domain.ResultPattern.Result;
 
 namespace Domain.Interfaces.Identity;
 
 public interface IIdentityService
 {
-    Task<(bool Succeeded, string? Error, int? UserId)> ExternalLoginAsync(
+    Task<Result<int>> ExternalLoginAsync(
         string provider,
         string providerKey,
         string email,
         string? firstName,
         string? lastName,
-        string? avatarUrl,
+        string? password,
         CancellationToken cancellationToken = default
     );
     Task<bool> IsEmailConfirmedAsync(int userId);
-    Task<User> GetUserByEmailAsync(string email);
-    Task<User> GetUserByIdAsync(int id);
-    Task<User> RegisterUserAsync(User user);
+    Task<User?> GetUserByEmailAsync(string email);
+    Task<User?> GetUserByIdAsync(int id);
+    Task<Result<int>> RegisterUserAsync(
+        string email,
+        string firstName,
+        string lastName,
+        string password,
+        CancellationToken cancellationToken = default
+    );
 }
