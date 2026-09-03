da302b3457d83c55ffa13a0f26acb8c51f41ed03
1111-ssss <gurskievgeni@gmail.com>
2026-09-03 14:34:38 +0300

    feature(database): add ardalis package and register repository

--- src/Web/Extensions/DatabaseConfigurationExtensions.cs
+++ src/Web/Extensions/DatabaseConfigurationExtensions.cs
@@ -1,4 +1,6 @@
 using Infrastructure.Database.DependencyInjection;
+using Ardalis.Specification.EntityFrameworkCore;
+using Ardalis.Specification;
 
 namespace Web.Extensions;
 
@@ -7,6 +9,8 @@ public static class DatabaseConfigurationExtensions
     public static IServiceCollection AddDatabaseConfiguration(this IServiceCollection services, IConfiguration configuration)
     {
         services.ConfigureDbContext(configuration);
+
+        services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
 
         return services;
     }
