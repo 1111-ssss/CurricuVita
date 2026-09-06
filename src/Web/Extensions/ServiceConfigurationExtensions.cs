cefb37f1d28883feb07d496869f7e4e386054378
1111-ssss <gurskievgeni@gmail.com>
2026-09-06 21:23:44 +0300

    feature(web): implement service extensions

--- src/Web/Extensions/ServiceConfigurationExtensions.cs
+++ src/Web/Extensions/ServiceConfigurationExtensions.cs
@@ -1,23 +1,55 @@
 using Application;
-using Infrastructure.Interfaces;
+using Domain.Interfaces.Services;
+using Domain.Options;
+using FluentEmail.MailKitSmtp;
 using Infrastructure.Services;
+using Web.BackgroundServices;
 
 namespace Web.Extensions;
 
 public static class ServiceConfigurationExtensions
 {
-    public static IServiceCollection AddServiceConfiguration(this IServiceCollection services)
+    public static IServiceCollection AddServiceConfiguration(this IServiceCollection services, IConfiguration configuration)
     {
+        // Options
+        services.Configure<CloudinaryOptions>(
+            configuration.GetSection(CloudinaryOptions.SectionName)
+        );
+        services.Configure<EmailSenderOptions>(
+            configuration.GetSection(EmailSenderOptions.SectionName)
+        );
+
+        // Razor Components
         services.AddRazorComponents()
             .AddInteractiveServerComponents();
 
-        // Infrastructure services
-        services.AddScoped<IDatabaseSeeder, DatabaseSeeder>();
+        // Logging
+        services.AddLogging();
         
         // MediatR
         services.AddMediatR(cfg =>
             cfg.RegisterServicesFromAssembly(typeof(AssemblyMarker).Assembly)
         );
+
+        // Services
+        services.AddScoped<IEmailSenderService, EmailSenderService>();
+
+        // Background Services
+        services.AddHostedService<EmailBackgroundService>();
+        
+        // Email Configuration
+        var emailSection = configuration.GetSection("Email");
+        services
+            .AddFluentEmail(emailSection["From"])
+            .AddMailKitSender(new SmtpClientOptions
+            {
+                Server = emailSection["SmtpServer"],
+                Port = int.Parse(emailSection["Port"]!),
+                UseSsl = true,
+                RequiresAuthentication = true,
+                User = emailSection["From"],
+                Password = emailSection["Password"]
+            });
 
         return services;
     }
