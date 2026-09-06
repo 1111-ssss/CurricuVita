a23db1dba4c2b738f75a5ab1bbcd73d904d85f57
1111-ssss <gurskievgeni@gmail.com>
2026-09-06 21:13:35 +0300

    feature(email-queue): implement email queue service

---
+++ src/Infrastructure/Services/EmailQueueService.cs
@@ -0,0 +1,35 @@
+using System.Threading.Channels;
+using Domain.Contracts;
+using Infrastructure.Interfaces;
+using Infrastructure.Constants;
+
+namespace Infrastructure.Services;
+
+public class EmailQueueService : IEmailQueueService
+{
+    private readonly Channel<EmailMessageRequest> _queue;
+
+    public EmailQueueService()
+    {
+        var options = new BoundedChannelOptions(EmailQueueServiceConstants.EmailQueueCapacity)
+        {
+            FullMode = BoundedChannelFullMode.Wait
+        };
+        _queue = Channel.CreateBounded<EmailMessageRequest>(options);
+    }
+
+    public async ValueTask EnqueueEmail(
+        EmailMessageRequest request,
+        CancellationToken cancellationToken = default
+    )
+    {
+        await _queue.Writer.WriteAsync(request, cancellationToken);
+    }
+
+    public async ValueTask<EmailMessageRequest> DequeueEmail(
+        CancellationToken cancellationToken
+    )
+    {
+        return await _queue.Reader.ReadAsync(cancellationToken);
+    }
+}
