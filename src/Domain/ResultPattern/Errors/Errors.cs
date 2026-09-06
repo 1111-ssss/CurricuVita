4eaf9d7c62c7a9ea8c4cebbb3210a0a9dff8c886
1111-ssss <gurskievgeni@gmail.com>
2026-09-06 20:51:16 +0300

    feature(result): add errors

--- src/Domain/ResultPattern/Errors/Errors.cs
+++ src/Domain/ResultPattern/Errors/Errors.cs
@@ -6,5 +6,11 @@ namespace Domain.ResultPattern.Errors;
 public static class Errors
 {
     public static Error ExternalLoginError = new(HttpStatusCode.BadRequest, "ExternalLoginError", "External login error.");
-    
+    public static Error InvalidCredentials = new(HttpStatusCode.BadRequest, "InvalidCredentials", "Invalid credentials.");
+    public static Error EmailNotConfirmed = new(HttpStatusCode.BadRequest, "EmailNotConfirmed", "Email not confirmed.");
+    public static Error UserLockedOut = new(HttpStatusCode.BadRequest, "UserLockedOut", "User locked out.");
+    public static Error LoginNotAllowed = new(HttpStatusCode.BadRequest, "LoginNotAllowed", "Login not allowed.");
+    public static Error Unauthorized = new(HttpStatusCode.Unauthorized, "Unauthorized", "Unauthorized.");
+    public static Error NotFound = new(HttpStatusCode.NotFound, "NotFound", "Not found.");
+    public static Error AvatarUploadError = new(HttpStatusCode.BadRequest, "AvatarUploadError", "Avatar upload error.");
 }
