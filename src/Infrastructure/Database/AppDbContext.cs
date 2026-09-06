8691a8e53c54fa3a312ca28e130cca15fbaeca74
1111-ssss <gurskievgeni@gmail.com>
2026-09-06 20:48:57 +0300

    refactor(entities): remove identity user from infrastructure

--- src/Infrastructure/Database/AppDbContext.cs
+++ src/Infrastructure/Database/AppDbContext.cs
@@ -1,12 +1,11 @@
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Infrastructure.Database.Entities;
 using Microsoft.AspNetCore.Identity;
 
 namespace Infrastructure.Database;
 
-public partial class AppDbContext : IdentityDbContext<ApplicationUser, IdentityRole<int>, int>
+public partial class AppDbContext : IdentityDbContext<User, IdentityRole<int>, int>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
@@ -22,12 +21,13 @@ public partial class AppDbContext : IdentityDbContext<ApplicationUser, IdentityR
     public DbSet<Project> Projects { get; set; }
     public DbSet<ProjectTag> ProjectTags { get; set; }
     public DbSet<Tag> Tags { get; set; }
-    public DbSet<User> AppUsers { get; set; }
     public DbSet<UserAttributeValue> UserAttributeValues { get; set; }
     public DbSet<UserBadge> UserBadges { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
         OnModelCreatingPartial(modelBuilder);
     }
