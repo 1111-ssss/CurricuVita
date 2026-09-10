8cbc2dff6c951843bc81ef84e634e2fee448ff59
1111-ssss <gurskievgeni@gmail.com>
2026-09-10 13:23:33 +0300

    fix(auth): fix endpoints

--- src/Web/Endpoints/AuthEndpoints.cs
+++ src/Web/Endpoints/AuthEndpoints.cs
@@ -35,11 +35,10 @@ public static class AuthEndpoints
 
     private static async Task<IResult> Logout(
         [FromServices] IMediator mediator,
-        LogoutRequest request,
         CancellationToken cancellationToken = default
     )
     {
-        await mediator.Send(request, cancellationToken);
+        await mediator.Send(new LogoutRequest(), cancellationToken);
 
         return Results.Unauthorized();
     }
