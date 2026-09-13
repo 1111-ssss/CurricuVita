eee81f2890177971b1142b071ad82b27944d2c16
1111-ssss <gurskievgeni@gmail.com>
2026-09-13 20:45:29 +0300

    feature(projects): impelent update project

---
+++ src/Application/Features/Projects/UpdateProject/UpdateProjectCommandValidator.cs
@@ -0,0 +1,51 @@
+using FluentValidation;
+
+namespace Application.Features.Projects.UpdateProject;
+
+public class UpdateProjectCommandValidator : AbstractValidator<UpdateProjectCommand>
+{
+    public UpdateProjectCommandValidator()
+    {
+        RuleFor(x => x.ProjectId)
+            .GreaterThan(0)
+            .WithMessage("Project is required.")
+            .WithErrorCode("ValidationFailed");
+
+        RuleFor(x => x.UserId)
+            .GreaterThan(0)
+            .WithMessage("User is required.")
+            .WithErrorCode("ValidationFailed");
+
+        RuleFor(x => x.Title)
+            .NotEmpty()
+            .WithMessage("Title is required.")
+            .WithErrorCode("ValidationFailed");
+        RuleFor(x => x.Title)
+            .MaximumLength(200)
+            .WithMessage("Title must be at most 200 characters.")
+            .WithErrorCode("ValidationFailed");
+
+        RuleFor(x => x.DescriptionMarkdown)
+            .MaximumLength(20000)
+            .WithMessage("Description must be at most 20000 characters.")
+            .WithErrorCode("ValidationFailed");
+
+        RuleFor(x => x.Tags)
+            .NotNull()
+            .WithMessage("Tags are required.")
+            .WithErrorCode("ValidationFailed");
+        RuleFor(x => x.Tags.Count)
+            .LessThanOrEqualTo(20)
+            .WithMessage("At most 20 tags are allowed.")
+            .WithErrorCode("ValidationFailed");
+        RuleForEach(x => x.Tags)
+            .MaximumLength(100)
+            .WithMessage("Tag must be at most 100 characters.")
+            .WithErrorCode("ValidationFailed");
+
+        RuleFor(x => x.Version)
+            .GreaterThanOrEqualTo(1)
+            .WithMessage("Version is required.")
+            .WithErrorCode("ValidationFailed");
+    }
+}
